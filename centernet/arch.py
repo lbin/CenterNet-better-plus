a4afc0eb372ecdcde3e32551edfcb836c57c08e2
Li Bin <lbin@outlook.com>
2020-03-02 11:08:46 +0800

    rm loss file

--- centernet/arch.py
+++ centernet/arch.py
@@ -3,16 +3,52 @@ import math
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from detectron2.modeling.backbone import build_backbone
 from detectron2.modeling.meta_arch.build import META_ARCH_REGISTRY
 from detectron2.structures import Boxes, ImageList, Instances
 
-from .centernet_decode import CenterNetDecoder
+from .centernet_decode import CenterNetDecoder, gather_feature
 from .centernet_deconv import CenternetDeconv
 from .centernet_gt import CenterNetGT
 from .centernet_head import CenternetHead
-from .focal_loss import modified_focal_loss
-from .reg_l1_loss import reg_l1_loss
+
+__all__ = ["CenterNet"]
+
+
+def reg_l1_loss(output, mask, index, target):
+    pred = gather_feature(output, index, use_transform=True)
+    mask = mask.unsqueeze(dim=2).expand_as(pred).float()
+    # loss = F.l1_loss(pred * mask, target * mask, reduction='elementwise_mean')
+    loss = F.l1_loss(pred * mask, target * mask, reduction="sum")
+    loss = loss / (mask.sum() + 1e-4)
+    return loss
+
+
+def modified_focal_loss(pred, gt):
+    """
+    focal loss copied from CenterNet, modified version focal loss
+    change log: numeric stable version implementation
+    """
+    pos_inds = gt.eq(1).float()
+    neg_inds = gt.lt(1).float()
+
+    neg_weights = torch.pow(1 - gt, 4)
+    # clamp min value is set to 1e-12 to maintain the numerical stability
+    pred = torch.clamp(pred, 1e-12)
+
+    pos_loss = torch.log(pred) * torch.pow(1 - pred, 2) * pos_inds
+    neg_loss = torch.log(1 - pred) * torch.pow(pred, 2) * neg_weights * neg_inds
+
+    num_pos = pos_inds.float().sum()
+    pos_loss = pos_loss.sum()
+    neg_loss = neg_loss.sum()
+
+    if num_pos == 0:
+        loss = -neg_loss
+    else:
+        loss = -(pos_loss + neg_loss) / num_pos
+    return loss
 
 
 def build_upsample_layers(cfg,):
@@ -23,9 +59,6 @@ def build_upsample_layers(cfg,):
 def build_head(cfg,):
     head = CenternetHead(cfg)
     return head
-
-
-__all__ = ["CenterNet"]
 
 
 @META_ARCH_REGISTRY.register()
